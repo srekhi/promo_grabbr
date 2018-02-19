ccd41b6595fb80fc338de645f80cd00b3f5ae4ff
Bill Xu <billxu93@gmail.com>
2018-02-18 19:12:04 -0800

    should be key instead of token

--- frontend/actions/auth_actions.js
+++ frontend/actions/auth_actions.js
@@ -3,7 +3,7 @@ import * as APIUtil from '../util/auth_api';
 export const googleLogin = code => dispatch => (
     APIUtil.googleAuth(code).then(res => {
         console.log(res);
-        localStorage.setItem('token', res.token);
+        localStorage.setItem('token', res.key);
     }, err => {
         console.log(err);
     })
