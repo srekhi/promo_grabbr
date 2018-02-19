8ce4ec747ca7cf1e3a723933cb79ea35196916c5
Sunny Rekhi <rohit.rekhi@gmail.com>
2018-02-18 17:51:51 -0800

    Change video in background

--- frontend/components/splash_page/video_background.jsx
+++ frontend/components/splash_page/video_background.jsx
@@ -3,6 +3,9 @@ const VideoBackground = () => {
   return (
     <div>
        <video id="background-video" loop autoPlay muted>
+		<source src="http://res.cloudinary.com/drpwlar6o/video/upload/v1519005037/Market.mp4" type="video/mp4" />Your browser does not support the video tag.
+		<source src="http://res.cloudinary.com/drpwlar6o/video/upload/v1519005054/Market.webm" type="video/webm" />Your browser does not support the video tag.
+  
       <source src={'https://res.cloudinary.com/dbbzpmyvc/video/upload/v1494882565/background_video_mrwna2.mp4'} type="video/mp4" />
             Your browser does not support the video tag.
       </video>
