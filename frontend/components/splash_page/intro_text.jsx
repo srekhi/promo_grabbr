53c01697e2f3bddad54da708adcf63464d271f03
Sunny Rekhi <rohit.rekhi@gmail.com>
2018-02-19 19:33:20 -0800

    Revamp homepage styles (wip)

---
+++ frontend/components/splash_page/intro_text.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './intro-text.css';
+
+const IntroText = () => (
+	<section className='intro-text'>
+		<h1>We know the feeling.</h1>
+		<br />
+		<p className='intro-quotation'><i>"J-Crew had a 50% store-wide sale?! Postmates had free delivery this week?!"</i></p>
+		<br />
+		<h2>Don't let that happen again. We'll let you know when there's a deal too good to miss, so you don't have to go through all your spam email to find it.</h2>
+	</section>
+);
+
+export default IntroText;
