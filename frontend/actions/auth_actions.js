2a1d7f52fd199db57be512e8303354d43dd6516d
Bill Xu <billxu93@gmail.com>
2018-03-04 20:11:38 -0800

    cleanup google auth sigin

--- frontend/actions/auth_actions.js
+++ frontend/actions/auth_actions.js
@@ -2,7 +2,6 @@ import * as APIUtil from '../util/auth_api';
 
 export const googleLogin = code => dispatch => (
     APIUtil.googleAuth(code).then(res => {
-        console.log(res);
         localStorage.setItem('token', res.key);
     }, err => {
         console.log(err);
